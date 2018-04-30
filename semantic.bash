84c48d9168193477c45e77dda324484edb45c58d
suyang98 <458144650@qq.com>
2018-04-30 16:28:41 +0800

    update

--- semantic.bash
+++ semantic.bash
@@ -5,6 +5,6 @@
 
 set -e
 cd "$(dirname "$0")"
-export CCHK="java -classpath ./lib/antlr-4.7.1-complete.jar:./bin compiler.src.main.java.main"
+export CCHK="java -classpath ./lib/antlr-4.7.1-complete.jar:./bin main.java.main"
 cat > program.txt   # save everything in stdin to program.txt
 $CCHK
