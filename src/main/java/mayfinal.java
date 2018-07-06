157c749b31dfff6e30457d87641819f642fe8fe6
suyang98 <458144650@qq.com>
2018-07-06 21:10:35 +0800

    delete label

--- src/main/java/mayfinal.java
+++ src/main/java/mayfinal.java
@@ -952,7 +952,7 @@ public class mayfinal {
     void optim(){
         for (int i = 0; i < print_list.size(); ++i){
             p t = print_list.get(i);
-            if (t instanceof sent && ((sent)t).operation.indexOf("j") != -1)
+            if (t instanceof sent && (((sent)t).operation.indexOf("j") != -1 || ((sent) t).operation.equals("call")))
                 label_map.get(((sent) t).s1+":").appear.add(i);
         }
         for (Object obj:label_map.keySet()){
